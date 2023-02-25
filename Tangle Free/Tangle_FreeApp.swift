ecefe72b8332fb26d091518fadfaf4e9e7700805
djuarez2652 <85270302+djuarez2652@users.noreply.github.com>
2023-02-25 03:29:57 -0500

    Initial Commit

---
+++ Tangle Free/Tangle_FreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tangle_FreeApp.swift
+//  Tangle Free
+//
+//  Created by David Juarez on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Tangle_FreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
