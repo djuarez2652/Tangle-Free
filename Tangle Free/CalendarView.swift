5b56a4d137ce2739666deea8eb712487afe09d00
djuarez2652 <85270302+djuarez2652@users.noreply.github.com>
2023-02-25 23:28:28 -0500

    Update CalendarView.swift

--- Tangle Free/CalendarView.swift
+++ Tangle Free/CalendarView.swift
@@ -8,10 +8,23 @@
 import SwiftUI
 
 struct CalendarView: View {
+    @State var selectedDate: Date = Date()
     var body: some View {
-        VStack {
+        VStack () {
             Text(selectedDate.formatted(date: .abbreviated, time: .omitted))
+                .font(.system(size: 20))
+                .bold()
+                .foregroundColor(Color.accentColor)
+                .padding()
+                .animation(.spring(), value: selectedDate)
+                .frame(width: 500)
+            Divider().frame(height: 1)
+            DatePicker("Select Date", selection: $selectedDate, displayedComponents: [.date])
+                .padding(.horizontal)
+                .datePickerStyle(.graphical)
+            Divider()
         }
+        .padding(.vertical, 100)
     }
 }
 
