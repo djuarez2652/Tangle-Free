751f683a726cdaa58c5cdb715775095051455f2f
djuarez2652 <85270302+djuarez2652@users.noreply.github.com>
2023-02-25 23:20:08 -0500

    Update CalendarView.swift

--- Tangle Free/CalendarView.swift
+++ Tangle Free/CalendarView.swift
@@ -9,35 +9,9 @@ import SwiftUI
 
 struct CalendarView: View {
     var body: some View {
-        Color.blue
-        
-//        TabView {
-//            MainView()
-//                .tabItem {
-//                    Image(systemName: "house")
-//                    Text("Home")
-//                }
-//            CalendarView()
-//                .tabItem {
-//                    Image(systemName: "calendar")
-//                    Text("Calendar")
-//                }
-//            MessageView()
-//                .tabItem {
-//                    Image(systemName: "message")
-//                    Text("Messages")
-//                }
-//            ServicesView()
-//                .tabItem {
-//                    Image(systemName: "bag")
-//                    Text("Services")
-//                }
-//            AccountView()
-//                .tabItem {
-//                    Image(systemName: "person")
-//                    Text("Account")
-//                }
-//        }
+        VStack {
+            Text(selectedDate.formatted(date: .abbreviated, time: .omitted))
+        }
     }
 }
 
