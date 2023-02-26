6b345a00d251c632bcd8733f7ae36a8bff23d54a
djuarez2652 <85270302+djuarez2652@users.noreply.github.com>
2023-02-26 03:35:16 -0500

    Update CalendarView.swift

--- Tangle Free/CalendarView.swift
+++ Tangle Free/CalendarView.swift
@@ -10,22 +10,35 @@ import SwiftUI
 struct CalendarView: View {
     @State var selectedDate: Date = Date()
     var body: some View {
-        VStack () {
-            Text(selectedDate.formatted(date: .abbreviated, time: .omitted))
-                .font(.system(size: 20))
-                .bold()
-                .foregroundColor(Color.accentColor)
-                .padding()
-                .animation(.spring(), value: selectedDate)
-                .frame(width: 500)
-            Divider().frame(height: 1)
+        VStack {
+            
+//            Text(selectedDate.formatted(date: .abbreviated, time: .omitted))
+//                .font(.system(size: 20))
+//                .bold()
+//                .foregroundColor(Color.accentColor)
+//                .padding()
+//                .animation(.spring(), value: selectedDate)
+//                .frame(width: 500)
+//            Divider().frame(height: 1)
             DatePicker("Select Date", selection: $selectedDate, displayedComponents: [.date])
                 .padding(.horizontal)
                 .datePickerStyle(.graphical)
             Divider()
+            
+            HStack {
+                Text("Upcoming Appointments")
+                    .fontWeight(.bold)
+                    .font(.title2)
+                    .padding(.top, 10)
+                    .padding(.leading, 25)
+                Spacer()
+            }
+                
             Spacer()
         }
         .padding(.vertical, 100)
+        
+        
     }
     
 }
